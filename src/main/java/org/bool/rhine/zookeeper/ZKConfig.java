5e408c775175f3781bbbe309436f73cfd54c51c4
dongmaosen <dongmaosen@yeah.net>
2018-08-12 22:23:06 +0800

    zk工具

---
+++ src/main/java/org/bool/rhine/zookeeper/ZKConfig.java
@@ -0,0 +1,59 @@
+package org.bool.rhine.zookeeper;
+/**
+ * zookeeper配置参数
+ * @author 不二
+ *
+ */
+public class ZKConfig {
+	
+	private String connectString;
+	
+	private String path;
+	
+	private int sessionTimeout;
+	
+	private String userName;
+	
+	private String password;
+
+	public String getConnectString() {
+		return connectString;
+	}
+
+	public void setConnectString(String connectString) {
+		this.connectString = connectString;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public int getSessionTimeout() {
+		return sessionTimeout;
+	}
+
+	public void setSessionTimeout(int sessionTimeout) {
+		this.sessionTimeout = sessionTimeout;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
--- src/main/java/org/bool/rhine/zookeeper/ZKManager.java
+++ src/main/java/org/bool/rhine/zookeeper/ZKManager.java
@@ -1,4 +1,9 @@
 package org.bool.rhine.zookeeper;
+
+import java.util.ResourceBundle;
+
+import org.apache.zookeeper.ZooKeeper;
+
 /**
  * ZK管理器，初始化配置等
  *
@@ -8,10 +13,36 @@ package org.bool.rhine.zookeeper;
  * 
  */
 public class ZKManager {
+	
 	/**
-	 * init zk config
+	 * ZK的配置
 	 */
-	public void init() {
-		
+	private static ZKConfig zkConfig;
+	
+	/**
+	 * ZK客户端对象
+	 */
+	private static ZooKeeper zookeeper;
+	
+	/**
+	 * initialize zkConfig
+	 */
+	public static void initConfig() {
+		if (zkConfig == null) {
+			zkConfig = new ZKConfig();
+			ResourceBundle rb = ResourceBundle.getBundle("zk.properties");
+			zkConfig.setConnectString(rb.getString("connectString"));
+			zkConfig.setPath(rb.getString("path"));
+			zkConfig.setSessionTimeout(Integer.parseInt(rb.getString("sessionTimeout")));
+			zkConfig.setUserName(rb.getString("userName"));
+			zkConfig.setPassword(rb.getString("password"));
+		}
+	}
+	
+	public static ZKConfig getZKConfig() {
+		if (zkConfig == null) {
+			initConfig();
+		}
+		return zkConfig;
 	}
 }
