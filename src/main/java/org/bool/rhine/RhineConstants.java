71af2387b0b1a60152e2e525406d409fd88a23d2
dongmaosen <dongmaosen@yeah.net>
2019-03-13 10:20:49 +0800

    配置文件可见性调整

---
+++ src/main/java/org/bool/rhine/RhineConstants.java
@@ -0,0 +1,12 @@
+package org.bool.rhine;
+/**
+ * rhine constants
+ *
+ * Author: 不二   
+ *
+ * Copyright @ 2019
+ * 
+ */
+public class RhineConstants {
+	public static final String INIT_SUCCESS= "INIT_SUCCESS";
+}
